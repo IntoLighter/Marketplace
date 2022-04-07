5f12034cf13bc8ff599c51de6a6e3279d4832d9a
Andrey Alexeev <IntoLighter.net@gmail.com>
2022-04-07 12:43:13 +0700

    Update domains

--- Infrastructure/Identity/AppUser.cs
+++ Infrastructure/Identity/AppUser.cs
@@ -1,10 +1,8 @@
-using Domain.Cart;
 using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Identity;
 
 public class AppUser : IdentityUser
 {
-    public List<CartProduct> CartProducts { get; set; } = new();
     public string? ImageUri { get; set; }
 }
