c8dfb112ccbf28b412a7bac900cc095e0308b32b
malfoyz <the13mufasa@gmail.com>
2022-04-09 21:11:00 +0700

    Добавил partial view для теста

--- Web/Areas/Search/Pages/Catalog.cshtml.cs
+++ Web/Areas/Search/Pages/Catalog.cshtml.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Marketplace;
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,17 @@ namespace Web.Areas.Search.Pages
 {
     public class CatalogModel : PageModel
     {
+        private readonly AppDbContext _context;
+        public List<Dish> Dishes { get; set; }
+        public List<Product> Products { get; set; }
+        public CatalogModel(AppDbContext db)
+        {
+            _context = db;
+        }
         public void OnGet()
         {
+            Products = _context.Products.ToList();
+            Dishes = _context.Dishes.ToList();
         }
     }
 }
